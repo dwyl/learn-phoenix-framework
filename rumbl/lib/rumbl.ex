39d926dad10a7b969c7ae4e1b43d1e3bdde0b46d
nelsonic <contact.nelsonic+github@gmail.com>
2017-02-05 23:57:43 +0000

    comment out the supervisor(Rumbl.Repo, []), from lib/rumbl.ex

--- rumbl/lib/rumbl.ex
+++ rumbl/lib/rumbl.ex
@@ -9,7 +9,7 @@ defmodule Rumbl do
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
-      supervisor(Rumbl.Repo, []),
+      # supervisor(Rumbl.Repo, []),
       # Start the endpoint when the application starts
       supervisor(Rumbl.Endpoint, []),
       # Start your own worker by calling: Rumbl.Worker.start_link(arg1, arg2, arg3)
