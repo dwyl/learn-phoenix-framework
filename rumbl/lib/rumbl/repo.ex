4c02ff4ad3b18a456b319f2b2542ae2d25e946bb
nelsonic <contact.nelsonic+github@gmail.com>
2017-02-05 23:55:56 +0000

    adds get and get_by functions to get a user by id or custom attribute. #2

--- rumbl/lib/rumbl/repo.ex
+++ rumbl/lib/rumbl/repo.ex
@@ -9,4 +9,15 @@ defmodule Rumbl.Repo do
      %Rumbl.User{id: 3, name: "Chris", username: "chrismccord", password: "phx"}]
   end
   def all(_module), do []
+
+  # get user by id
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, value} -> Map.get(map, key) == val end)
+    end
+  end
 end
