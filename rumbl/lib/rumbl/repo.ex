c851d45e43c8797e2ba7b7f69b58784a883ca0de
nelsonic <contact.nelsonic+github@gmail.com>
2017-02-05 23:52:36 +0000

    add (hard-coded) "users" to lib/rumbl/repo.ex

--- rumbl/lib/rumbl/repo.ex
+++ rumbl/lib/rumbl/repo.ex
@@ -3,4 +3,10 @@ defmodule Rumbl.Repo do
   @moduledoc """
   In memory repository
   """
+  def all(Rumbl.User) do
+    [%Rumbl.User{id: 1, name: "José", username: "josevalim", password: "elixir"},
+     %Rumbl.User{id: 2, name: "Bruce", username: "redrapids", password: "7langs"},
+     %Rumbl.User{id: 3, name: "Chris", username: "chrismccord", password: "phx"}]
+  end
+  def all(_module), do []
 end
