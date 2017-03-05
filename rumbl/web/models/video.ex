27c4a115fa9cac4429fa31ce53203931586f52f4
nelsonic <contact.nelsonic+github@gmail.com>
2017-03-05 14:12:17 +0000

    adds field :slug, :string to web/models/video.ex as suggested by @dogbert in http://stackoverflow.com/questions/42609234/defimpl-phoenix-param-to-override-to-param-causes-functionclauseerror-no-funct?noredirect=1#comment72348042_42609234

--- rumbl/web/models/video.ex
+++ rumbl/web/models/video.ex
@@ -7,7 +7,7 @@ defmodule Rumbl.Video do
     field :description, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
-
+    field :slug, :string
     timestamps() # invocation parenthesis not in book
   end
 
