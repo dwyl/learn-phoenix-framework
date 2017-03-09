0fcfc5849b4973bd144cd8ac9904faae14354adf
nelsonic <contact.nelsonic+github@gmail.com>
2017-03-09 06:19:04 +0000

    p.185 update web/channels/video_channel.ex to broacast all new_annotation messages to connected clients

--- rumbl/web/channels/video_channel.ex
+++ rumbl/web/channels/video_channel.ex
@@ -2,13 +2,17 @@ defmodule Rumbl.VideoChannel do
   use Rumbl.Web, :channel
 
   def join("videos:" <> video_id, _params, socket) do
-    :timer.send_interval(5_000, :ping)
+    # :timer.send_interval(5_000, :ping)
     {:ok, socket} #assign(socket, :video_id, String.to_integer(video_id))}
   end
 
-  def handle_info(:ping, socket) do
-    count = socket.assigns[:count] || 1
-    push socket, "ping", %{count: count}
-    {:noreply, assign(socket, :count, count + 1)}
+  def handle_in("new_annotation", params, socket) do
+    broadcast! socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    }
+
+    {:reply, :ok, socket}
   end
 end
--- rumbl/web/static/js/video.js
+++ rumbl/web/static/js/video.js
@@ -16,9 +16,39 @@ let Video = {
     let postButton = document.getElementById("msg-submit")
     let vidChannel = socket.channel("videos:" + videoId)
 
+    vidChannel.on("ping", ({count}) => console.log("PING", count))
+
+    postButton.addEventListener("click", e => {
+      let payload = {body: msgInput.value, at: Player.getCurrentTime()}
+      vidChannel.push("new_annotation", payload)
+        .receive("error", e => console.log(e))
+      msgInput.value = ""
+    })
+
+    vidChannel.on("new_annotation", (resp) => {
+      this.renderAnnotation(msgContainer, resp)
+    })
+
     vidChannel.join()
     .receive("ok", resp => console.log("joined the video channel", resp))
     .receive("error", reason => console.log("join failed", reason))
+  },
+
+  esc(str) {
+    let div = document.createElement("div")
+    div.appendChild(document.createTextNode(str))
+    return div.innerHTML
+  },
+
+  renderAnnotation(msgContainer, {user, body, at}) {
+    let template = document.createElement("div")
+    template.innerHTML = `
+    <a href="#" data-seek="${this.esc(at)}">
+      <b>${this.esc(user.username)}</b>: ${this.esc(body)}
+    </a>
+    `
+    msgContainer.appendChild(template)
+    msgContainer.scrollTop = msgContainer.scrollHeight
   }
 }
 
