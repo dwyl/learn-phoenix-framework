0ef500b1652d39d7a1b4d1f7fdde825edee539e8
nelsonic <contact.nelsonic+github@gmail.com>
2017-03-11 23:11:37 +0000

    p.223-224 adds timer (to avoid waiting for ever for an API reponse) to info_sys process

--- rumbl/lib/rumbl/info_sys.ex
+++ rumbl/lib/rumbl/info_sys.ex
@@ -28,8 +28,12 @@ defmodule Rumbl.InfoSys do
     {pid, monitor_ref, query_ref}
   end
 
-  defp await_results(children, _opts) do
-    await_result(children, [], :infinity)
+  defp await_results(children, opts) do
+    timeout = opts[:timeout] || 5000
+    timer = Process.send_after(self(), :timeout, timeout)
+    results = await_result(children, [], :infinity)
+    cleanup(timer)
+    results
   end
 
   defp await_result([head|tail], acc, timeout) do
@@ -41,10 +45,31 @@ defmodule Rumbl.InfoSys do
         await_result(tail, results ++ acc, timeout)
       {:DOWN, ^monitor_ref, :process, ^pid, _reason} ->
         await_result(tail, acc, timeout)
+      :timeout ->
+        kill(pid, monitor_ref)
+        await_result(tail, acc, 0)
+    after
+      timeout ->
+        kill(pid, monitor_ref)
+        await_result(tail, acc, 0)
     end
   end
 
   defp await_result([], acc, _) do
     acc
+  end
+
+  defp kill(pid, ref) do
+    Process.demonitor(ref, [:flush])
+    Process.exit(pid, :kill)
+  end
+
+  defp cleanup(timer) do
+    :erlang.cancel_timer(timer)
+    receive do
+      :timeout -> :ok
+    after
+      0 -> :ok
+    end
   end
 end
