04313bd160bc6fed8a9af518cc2e456dee6d0eea
nelsonic <contact.nelsonic+github@gmail.com>
2017-03-11 21:58:35 +0000

    p.215 adds start_link Proxy setup code

---
+++ rumbl/lib/rumbl/info_sys.ex
@@ -0,0 +1,26 @@
+defmodule Rumbl.InfoSys do
+  @backends [Rumbl.InfoSys.Wolfram]
+
+  defmodule Result do
+    defstruct score: 0, text: nul, url: nil, backend: nil
+  end
+
+  def start_link(backend, query, query_ref, owner, limit) do
+    backend.start_link(query, query_ref, owner, limit)
+  end
+
+  def compute(query, opts \\ []) do
+    limit = opts[:limit] || 10
+    backends = opts[:backends] || @backends
+
+    backends
+    |> Enum.map(&spawn_query(&1, query, limit))
+  end
+
+  defp spawn_query(backend, query, limit) do
+    query_ref = make_ref()
+    opts = [backend, query, query_ref, self(), limit]
+    {:ok, pid} = Supervisor.start_child(Rumbl.InfoSys.Supervisor, opts)
+    {pid, query_ref}
+  end
+end
---
+++ rumbl/lib/rumbl/info_sys/supervisor.ex
@@ -0,0 +1,15 @@
+defmodule Rumbl.InfoSys.Supervisor do
+  use Supervisor
+
+  def start_link() do
+    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init(_opts) do
+    children = [
+      worker(Rumbl.InfoSys, [], restart: :temporary)
+    ]
+
+    supervise children, strategy: :simple_one_for_one
+  end
+end
