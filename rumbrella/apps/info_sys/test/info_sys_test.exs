d2858c38021c01f48736a773add17d3254714983
nelsonic <contact.nelsonic+github@gmail.com>
2017-03-13 06:08:36 +0000

    p.240 update apps/info_sys/test_info_sys_test.exs with mock implementation

--- rumbrella/apps/info_sys/test/info_sys_test.exs
+++ rumbrella/apps/info_sys/test/info_sys_test.exs
@@ -1,8 +1,25 @@
 defmodule InfoSysTest do
   use ExUnit.Case
-  doctest InfoSys
+  alias InfoSys.Result
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  defmodule TestBackend do
+    def start_link(query, ref, owner, limit) do
+      Task.start_link(__MODULE__, :fetch, [query, ref, owner, limit])
+    end
+    def fetch("result", ref, owner, __limit) do
+      send(owner, {:results, ref, [%Result{backend: "test", text: "result"}]})
+    end
+    def fetch("none", ref, owner, __limit) do
+      send(owner, {:results, ref, []})
+    end
+  end
+
+  test "compute/2 with backend results" do
+    assert [%Result{backend: "test", text: "result"}] =
+      InfoSys.compute("result", backends: [TestBackend])
+  end
+
+  test "compute/2 with no backend results" do
+    assert [] = InfoSys.compute("none", backends: [TestBackend])
   end
 end
