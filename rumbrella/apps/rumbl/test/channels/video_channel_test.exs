b207facf64969a2740d28252a2872c8503067193
nelsonic <contact.nelsonic+github@gmail.com>
2017-03-13 08:29:11 +0000

    p.256 finalise testing annotation triggers

--- rumbrella/apps/rumbl/test/channels/video_channel_test.exs
+++ rumbrella/apps/rumbl/test/channels/video_channel_test.exs
@@ -29,4 +29,13 @@ defmodule Rumbl.Channels.VideoChannelTest do
     assert_reply ref, :ok, %{}
     assert_broadcast "new_annotation", %{}
   end
+
+  test "new annotations triggers InfoSys", %{socket: socket, video: vid} do
+    insert_user(username: "wolfram")
+    {:ok, _, socket} = subscribe_and_join(socket, "videos:#{vid.id}", %{})
+    ref = push socket, "new_annotation", %{body: "1 + 1", at: 123}
+    assert_reply ref, :ok, %{}
+    assert_broadcast "new_annotation", %{body: "1 + 1", at: 123}
+    assert_broadcast "new_annotation", %{body: "2", at: 123}
+  end
 end
